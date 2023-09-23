ffd359a6c685667e349373c8b5201372d0d21826
xxrtemy <rebelgamechannel@gmail.com>
2023-09-23 14:39:28 +0300

    corrected nav 2.0

--- scripts/nav-btn.js
+++ scripts/nav-btn.js
@@ -1,12 +1,12 @@
 
-const nav = document.querySelector('#nav');
-const navBtn = document.querySelector('#nav-btn');
-const navBtnImg =document.querySelector('#nav-btn-img');
-
-navBtn.onclick = () => {
-    if (nav.classList.toggle('open')){
-        navBtnImg.src = "../img/header/cancel-svgrepo-com.svg";
-    } else{
-        navBtnImg.src = "../img/header/hamburger-menu-svgrepo-com.svg";
+    const nav = document.querySelector('#nav');
+    const navBtn = document.querySelector('#nav-btn');
+    const navBtnImg =document.querySelector('#nav-btn-img');
+    
+    navBtn.onclick = () => {
+        if (nav.classList.toggle('open')){
+            navBtnImg.src = "../img/header/cancel-svgrepo-com.svg";
+        } else{
+            navBtnImg.src = "../img/header/hamburger-menu-svgrepo-com.svg";
+        }
     }
-}
