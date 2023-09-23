709f73c60806d8e2e798125ce1e26e8ae5ec98e0
xxrtemy <rebelgamechannel@gmail.com>
2023-09-23 15:28:16 +0300

    corrected nav 2.1

--- scripts/nav-btn.js
+++ scripts/nav-btn.js
@@ -2,6 +2,7 @@
     const nav = document.querySelector('#nav');
     const navBtn = document.querySelector('#nav-btn');
     const navBtnImg =document.querySelector('#nav-btn-img');
+
     
     navBtn.onclick = () => {
         if (nav.classList.toggle('open')){
